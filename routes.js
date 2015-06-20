ffa9afce25bbee43038af3407ff3aaea70e4877d
Teddy <Teddy@mail.com>
2015-06-20 16:04:56 +1000

    before post is redirected

--- routes.js
+++ routes.js
@@ -1,14 +1,7 @@
-var bodyParser = require('body-parser')
-
 module.exports = function (app) {
-
-    // POST /api/users gets JSON bodies
     app.post('/api/postding',  function (req, res) {
-        if (!req.body)
-            res.send('inside post error');
-        else
-            res.send(req.body);
-
+        //do some schema validation
+        console.log('api/postding posted');
+        res.end('done');
     })
-
 }
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 // set up ======================================================================
-var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var port  	 = process.env.PORT || 8080; 				// set the port
+    var express  = require('express');
+var app      = express();                               // create our app w/ express
+var port     = process.env.PORT || 8080;                // set the port
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-//app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    //the code hits this point!
+    var data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+        req.rawBody = data;
+        console.log('from req.on',data);
 
-app.use(function(err, req, res, next){
-   console.log(err);
-    res.send(err);
+    });
+    console.log(data);
+    next();
 });
-// logic
-// }); after app.use(bodyParser());
-
+app.use(bodyParser.json());
 app.use(methodOverride());
 
 // routes ======================================================================
 require('./routes.js')(app);
-
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
